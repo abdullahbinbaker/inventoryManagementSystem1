eb6450b30b3309d3a623fc59a307ec3999e2258f
abdullahbinbaker <binbaker64@gmail.com>
2021-02-03 11:31:46 +0300

    implementing the UOW

---
+++ API's/Controllers/ItemController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly UnitOfWork uow;
+        private readonly IConfiguration _config;
+        public ItemController(UnitOfWork unitOfWork, IConfiguration config)
+        {
+            uow = unitOfWork;
+            _config = config;
+        }
+
+        //[HttpPost("UpdateHoliday")]
+        //public async Task<ActionResult<HolidayViewModel>> SearchForItwm(string itemNo)
+        //{
+        //}
+
+
+
+        }
+}
--- Infrastructure/Repository/ItemRepository.cs
+++ Infrastructure/Repository/ItemRepository.cs
@@ -15,15 +15,14 @@ namespace Infrastructure.Repository
             _context = context;
         }
 
-        public async Task<Item> RetreiveItemInformation(string itemNo)
+        public async Task<Item> GetItem(string itemNo)
         {
             return await _context.Set<Item>().Where(x=>x.ItemNo==itemNo).FirstOrDefaultAsync();
         }
 
-        public async Task<List<Item>> LoadBadeel(string itemNo)
+        public async Task<IEnumerable<Item>> GetBadeels(string itemNo)
         {
             return await _context.Set<Item>().Where(item => item.OriginalItemNo == itemNo).ToListAsync();
- 
         }
 
         public async Task<Item> AddNewItem(Item item)
@@ -33,9 +32,24 @@ namespace Infrastructure.Repository
             return newItem.Entity;
         }
 
-        //public async Task<Item> UpdateItem(Item item)
-        //{
+        
+        public void UpdateItem(Item item)
+        {
+            _context.Set<Item>().Update(item);
 
+        }
+          //  Item itemToUpdate = _context.Set<Item>().Where(x => x.ItemNo == item.ItemNo).FirstOrDefault();
+        //    if (itemToUpdate != null)
+        //    {
+        //        itemToUpdate.ItemNo = holiday.EmployeeId;
+        //        itemToUpdate.NameArabic = holiday.HolidayStartDate;
+        //        itemToUpdate.HolidayEndDate = holiday.HolidayEndDate;
+        //        itemToUpdate.HolidayStatus = holiday.HolidayStatus;
+        //        itemToUpdate.HolidayReason = holiday.HolidayReason;
+
+        //        await _context.SaveChangesAsync();
+        //        return toUpdate;
+        //    }
         //}
 
     }
